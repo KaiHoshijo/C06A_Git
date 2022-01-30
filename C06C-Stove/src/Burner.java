c7a9ee17b90dd7b35be936353af7d9a255aaf30e
Kai Miller <kaihoshijo003@gmail.com>
2022-01-29 21:39:30 -0700

    Created the plusButton and minusButton to change stove's settings

--- C06C-Stove/src/Burner.java
+++ C06C-Stove/src/Burner.java
@@ -4,11 +4,59 @@ public class Burner {
 	private Temperature myTemperature;
 	private Setting mySetting;
 	private int timer;
-	private static final int TIME_DURATION = 2;
+	
+	public static final int TIME_DURATION = 2;
+	
+	public Burner() {
+		mySetting = Setting.OFF;
+		myTemperature = Temperature.COLD;
+	}
 	
 	
 	public Temperature getMyTemperature() {
 		return myTemperature;
+	}
+
+
+	public void plusButton() {
+		switch (mySetting.toString()) {
+			case "---":
+				mySetting = Setting.LOW;
+				break;
+			case "--+":
+				mySetting = Setting.MEDIUM;
+				break;
+			case "-++":
+				mySetting = Setting.HIGH;
+				break;
+		}
+		
+	}
+
+
+	public void minusButton() {
+		switch (mySetting.toString()) {
+			case "+++":
+				mySetting = Setting.MEDIUM;
+				break;
+			case "-++":
+				mySetting = Setting.LOW;
+				break;
+			case "--+":
+				mySetting = Setting.OFF;
+				break;
+		}
+		
+	}
+
+
+	public void updateTemperature() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void display() {
+		
 	}
 	
 	
